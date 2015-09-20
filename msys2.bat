d9da9fc62533757d1befcb25902e75a0c56c11e8
Keith Maxwell <keith.maxwell@gmail.com>
2015-09-20 11:02:58 +0100

    Use MSYS2 as installed by AppVeyor

--- msys2.bat
+++ msys2.bat
@@ -1,18 +1,10 @@
-if not defined msys2 goto :error
+rem Add mirrors and automatically move to APPVEYOR_BUILD_FOLDER
 if not defined mirrors goto :error
 if not defined APPVEYOR_BUILD_FOLDER goto :error
-if not exist %msys2% (
-	appveyor DownloadFile ^
-	http://downloads.sourceforge.net/project/msys2/Base/x86_64/%msys2%.xz ^
-	|| goto :error
-	7z x %msys2%.xz > nul || goto :error
-	del %msys2%.xz || goto :error
-)
 if not exist %mirrors% (
 	appveyor DownloadFile http://repo.msys2.org/msys/x86_64/%mirrors% ^
 	|| goto :error
 )
-7z x -oC:\ %msys2% > nul || goto :error
 C:\msys64\usr\bin\cygpath.exe -u %APPVEYOR_BUILD_FOLDER% > work_directory ^
 || goto :error
 set /P work_directory= < work_directory || goto :error
@@ -23,15 +15,6 @@ set MSYSTEM=MSYS2 || goto :error
 (
 echo cd "${APPVEYOR_BUILD_FOLDER}" ^^^&^^^&
 echo pacman --upgrade --noconfirm "${mirrors}"
-) | C:\msys64\usr\bin\sh --login -s > nul 2>&1 || goto :error
-(
-echo pacman --sync --refresh ^^^&^^^&
-echo pacman --sync --noconfirm --needed ^
-	bash pacman msys2-runtime msys2-runtime-devel
-) | C:\msys64\usr\bin\sh --login -s >nul 2>&1 || goto :error
-(
-echo pacman --sync --noconfirm --sysupgrade ^^^&^^^&
-echo pacman --sync --needed --noconfirm base-devel mingw-w64-x86_64-gcc
 ) | C:\msys64\usr\bin\sh --login -s > nul 2>&1 || goto :error
 set MSYSTEM= || goto :error
 goto :EOF
